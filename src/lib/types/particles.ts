5cda330f1aa107c13e78d8a32fc47441c9837eb6
itsEzz <github.swore664@passmail.net>
2025-09-10 22:16:17 +0200

    Added particles component

---
+++ src/lib/types/particles.ts
@@ -0,0 +1,12 @@
+export interface Circle {
+	x: number;
+	y: number;
+	translateX: number;
+	translateY: number;
+	size: number;
+	alpha: number;
+	targetAlpha: number;
+	dx: number;
+	dy: number;
+	magnetism: number;
+}
