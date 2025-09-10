bd4faf67676c4369e98f59ecacb866d7b7a25f60
itsEzz <github.swore664@passmail.net>
2025-09-10 19:51:07 +0200

    Added theme toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
-	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import MoonIcon from '@lucide/svelte/icons/moon';
+	import SunIcon from '@lucide/svelte/icons/sun';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
+	import '../app.css';
 
 	let { children } = $props();
 </script>
@@ -9,4 +13,20 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<!-- Theme toggle button -->
+<div class="fixed top-4 right-4 z-50">
+	<Button variant="secondary" onclick={toggleMode}>
+		<SunIcon
+			class="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 !transition-all dark:scale-0 dark:-rotate-90"
+		/>
+		<MoonIcon
+			class="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 !transition-all dark:scale-100 dark:rotate-0"
+		/>
+		<span class="sr-only">Toggle theme</span>
+	</Button>
+</div>
+
+<ModeWatcher />
+<main class="min-h-screen">
+	{@render children?.()}
+</main>
