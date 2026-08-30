7c25400d5e4ffa4cb394df883bebd0d88d151c05
itsEzz <github.swore664@passmail.net>
2026-08-30 11:01:31 +0200

    feat: add Paraglide i18n and update tooling configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,10 @@
-import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
-import svelte from 'eslint-plugin-svelte';
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
 import path from 'node:path';
+import js from '@eslint/js';
+import svelte from 'eslint-plugin-svelte';
+import { defineConfig, includeIgnoreFile } from 'eslint/config';
+import globals from 'globals';
 import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
 
 const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
 
@@ -16,10 +14,10 @@ export default defineConfig(
 		ignores: ['src/lib/components/ui/**']
 	},
 	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
+	ts.configs.recommended,
+	svelte.configs.recommended,
 	prettier,
-	...svelte.configs.prettier,
+	svelte.configs.prettier,
 	{
 		languageOptions: { globals: { ...globals.browser, ...globals.node } },
 		rules: {
@@ -34,9 +32,13 @@ export default defineConfig(
 			parserOptions: {
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig
+				parser: ts.parser
 			}
 		}
+	},
+	{
+		// Override or add rule settings here, such as:
+		// 'svelte/button-has-type': 'error'
+		rules: {}
 	}
 );
