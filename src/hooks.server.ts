7957d24b45a09847c09178518bf5058255b15605
itsEzz <github.swore664@passmail.net>
2026-08-30 11:02:46 +0200

    feat: add internationalization support using Paraglide

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,20 @@
+import type { Handle, ServerInit } from '@sveltejs/kit';
+import { paraglideMiddleware } from '$lib/paraglide/server';
+import { getTextDirection } from '$lib/paraglide/runtime';
 import { getLogger } from '$lib/server/logger';
-import type { ServerInit } from '@sveltejs/kit';
+
+// creating a handle to use the paraglide middleware
+const paraglideHandle: Handle = ({ event, resolve }) =>
+	paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+		event.request = localizedRequest;
+		return resolve(event, {
+			transformPageChunk: ({ html }) => {
+				return html.replace('%lang%', locale).replace('%dir%', getTextDirection(locale));
+			}
+		});
+	});
+
+export const handle: Handle = paraglideHandle;
 
 export const init: ServerInit = async () => {
 	const logger = getLogger();
